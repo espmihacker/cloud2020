35a17b3eef0901f9289041c1f659d76a1fd75921
espmihacker <mihacker.mh@gmail.com>
2020-03-07 23:29:13 +0800

    13 spring cloud config end

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientApplication3355.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientApplication3355 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication3355.class, args);
+    }
+
+}
---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope   // POST实现刷新http://localhost:3355/actuator/refresh
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
